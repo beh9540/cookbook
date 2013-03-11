ce86aec529390d85b5adb4356e8acf1c413713e0
Blake <bhowell@hdcomm.com>
2013-03-11 17:18:36 -0400

    -Fix tests -Add remove functionality in list template -Fix update to require a valid id -Fix tests so there is a valid user

--- recipes/urls.py
+++ recipes/urls.py
@@ -7,16 +7,16 @@ from django.conf.urls import patterns, url
 
 
 urlpatterns = patterns('recipes.views',
-    #list views
+    #list URLs
     url(r'^$', 'home', name='home'),
     url(r'^user/$', 'my_recipes', name='recipe-user'),
     url(r'^favorites/$', 'my_favorites', name='recipe-fav'),
-    #details
+    #CRUD URLs
     url(r'^(?P<recipe_id>\d+)/$', 'get', name='recipe-detail'),
-    #Add view urls
     url(r'^add/$', 'add', name='recipe-new'),
-    url(r'^update/(?P<recipe>[0-9]*)/$', 'add', name='recipe-update'),
-    #AJAX urls
+    url(r'^update/(?P<recipe>[0-9]+)/$', 'add', name='recipe-update'),
+    url(r'^remove/$', 'remove', name='recipe-remove'),
+    #AJAX URLs
     url(r'^newIngredient$', 'new_ingredient_form', 
         name='recipe-new-ingredient'),
     url(r'^newRecipeStep$', 'new_recipe_step_form', name='recipe-new-step')
